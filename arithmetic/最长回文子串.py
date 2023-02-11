e00f0e1e78ac9bbb00ae64afb5701135c42cd5e7
liangbosen <liangbosen@fuzhi.ai>
2023-02-11 19:19:37 +0800

    算法提交

--- arithmetic/最长回文子串.py
+++ arithmetic/最长回文子串.py
@@ -10,40 +10,36 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         """
-        分析情况：
-            1. 长度为1自动返回
+        1. 中心扩散算法
+            1. 分析：如果s[i: j+1]为回文串，那么如果s[i-1] == s[j+1]的话，s[i-1: j+2]也是回文串，因此可以通过中心扩散求解
+                1. 中心扩散函数构建：考虑到奇偶特性，若是为奇数子串，则从中心一位开始扩散，若是偶数子串，则从中心两位开始扩散，
+                    1. 扩散的方式，采用双指针，此时双指针也可以方便支持判断奇数偶数的扩散
         :param s:
         :return:
         """
-        # 获取长度
-        size = len(s)
-        # 处理特殊
-        if size == 1:
-            return s
-        # 定义temp长度与返回长度
-        temp_len = 1
-        re_len = 1
-        # 定义初始下标
-        start = 0
-        # 状态表
-        dp = [[False for _ in range(size)] for _ in range(size)]
-        # 填充状态表
-        for j in range(1, size):
-            for i in range(j):
-                if j - i <= 2:
-                    if s[j] == s[i]:
-                        dp[i][j] = True
-                        temp_len = j - i + 1
-                else:
-                    if s[i] == s[j] and dp[i + 1][j - 1]:
-                        dp[i][j] = True
-                        temp_len = j - i + 1
-                if dp[i][j]:
-                    (re_len, start) = (temp_len, i) if re_len < temp_len else (re_len, start)
-        return s[start: start + re_len]
+
+        # 定义中心扩散函数
+        def center_diffusion(s, l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l + 1: r]
+
+
+        # 定义初始结果串
+        res = ''
+        # 遍历整个串
+        for i in range(len(s)):
+            odd_sub = center_diffusion(s, i, i)
+            even_sub = center_diffusion(s, i, i+1)
+            res = odd_sub if len(odd_sub) > len(res) else res
+            res = even_sub if len(even_sub) > len(res) else res
+
+
+        return res
 
 
 if __name__ == '__main__':
     so = Solution()
-    res = so.longestPalindrome('ccc')
+    res = so.longestPalindrome('cbbd')
     print(res)
